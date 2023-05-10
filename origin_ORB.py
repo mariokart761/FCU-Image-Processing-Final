050385aa966650d142ec1e0f5d8523937940ed82
mariokart761 <mariokart761@gmail.com>
2023-05-11 01:49:18 +0800

    特徵點提取基礎樣版

---
+++ origin_ORB.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+# 读取图像
+img = cv2.imread('testImg.png')
+
+# 初始化ORB检测器
+orb = cv2.ORB_create(edgeThreshold=0)
+
+# 调整ORB阈值参数
+max_feature = 20000
+orb.setMaxFeatures(max_feature)
+
+adaptive_threshold = [20, 15, 7, 20, 13, 20, 15, 7, 20, 13, 20, 15, 7, 20, 13, 20, 15, 7, 20, 13, 20, 15, 7, 20, 13]
+
+# 将原始图像分割成5*5个子区域
+m = 5
+height, width, _ = img.shape
+block_width = width // m
+block_height = height // m
+
+# 初始化一个与原始图像大小相同的空白图像
+img_with_keypoints = np.zeros_like(img)
+keypoint_num = 0
+# 逐个子区域进行处理
+for i in range(m):
+    for j in range(m):
+        # 获取子区域的坐标
+        x_start = i * block_width
+        x_end = (i + 1) * block_width
+        y_start = j * block_height
+        y_end = (j + 1) * block_height
+        
+        # 提取子区域
+        block = img[y_start:y_end, x_start:x_end]
+        
+        # 设置FastThreshold值
+        orb.setFastThreshold(adaptive_threshold[i * 5 + j])
+        
+        # 在子区域上检测特征点
+        keypoints = orb.detect(block, None)
+        keypoint_num += len(keypoints)
+        # 在子区域上绘制特征点
+        block_with_keypoints = cv2.drawKeypoints(block, keypoints, None, flags=0)
+        
+        # 将子区域的特征点图像贴回到完整图像上
+        img_with_keypoints[y_start:y_end, x_start:x_end] = block_with_keypoints
+
+print("FastThreshold:" + str(orb.getFastThreshold()))
+print("MaxFeatures:" + str(orb.getMaxFeatures()))
+print("ScoreType:" + str(orb.getScoreType()))
+print("keypoint_num:" + str(keypoint_num))
+
+cv2.imshow('Image with Keypoints', img_with_keypoints)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
