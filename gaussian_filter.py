e734b41b802a4c52a25612c7735f630b423aff5e
mariokart761 <mariokart761@gmail.com>
2023-05-13 23:14:19 +0800

    高斯濾波完成

---
+++ gaussian_filter.py
@@ -0,0 +1,16 @@
+import cv2
+
+def gaussian_filter(img_path, kernel_size = 3, sigma = 0):
+    img = cv2.imread(img_path)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gaussian_img = cv2.GaussianBlur(gray_img, (kernel_size, kernel_size), sigma)
+
+    return gaussian_img
+
+# if __name__ == '__main__':
+#     img_path = './img/testImg.png'
+#     gaussian_img = gaussian_filter(img_path)
+#     cv2.imshow('Gaussian Image', gaussian_img)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
