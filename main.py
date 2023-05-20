b543c18e590eba9d4b7066ee7563eba6100821e7
mariokart761 <mariokart761@gmail.com>
2023-05-20 15:51:34 +0800

    Done backend 3 experiments setting

--- main.py
+++ main.py
@@ -33,7 +33,9 @@ async def handle_request(img_content: UploadFile = File(...),
                          statusGaussianFilter: bool = Form(...),
                          statusBrightnessFixMethod: str = Form(...),
                          statusSharpen: bool = Form(...),
-                         statusAdaptiveThreshold: bool = Form(...),):
+                         statusAdaptiveThreshold: bool = Form(...),
+                         statusExperiment: str = Form(...),
+                         statusBrightAdj: str = Form(...),):
     try:
         image_bytes = await img_content.read()
         decode_image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
@@ -46,23 +48,102 @@ async def handle_request(img_content: UploadFile = File(...),
             "statusAdaptiveThreshold": statusAdaptiveThreshold
         }
         print(config_data)
-
-        orbProcessor = orb_module.OrbProcessor()
-        orbProcessor.setORBProcessor(config_data)
-        result_img, keypoint_num, detect_time = orbProcessor.detectOrbFeature()
-        print("Features : " + str(keypoint_num))
-        print("Time : " + str(detect_time) + "s")
-
-        _, buffer = cv2.imencode('.jpg', result_img)
-        image_str = base64.b64encode(buffer).decode('utf-8')
-        content = {"processedImage": image_str,
-                   "keypointNum":keypoint_num,
-                   "detectTime":detect_time
-                }
+        experiment = statusExperiment
+        bright_adj = statusBrightAdj
+        response_wait_time = 1 # server回應延遲
         
-        time.sleep(1)
-        
-        return JSONResponse(content)
+        # 論文實驗1：相比原始算法提升了多少
+        if (experiment == "exp1"):
+            orbProcessor = orb_module.OrbProcessor()
+            orbProcessor.setORBProcessor(config_data)
+            result_img, keypoint_data, used_time = orbProcessor.compareToDefault()
+            """
+            result_img[0]: 左圖 detect結果圖
+            result_img[1]: 右圖 detect結果圖
+            result_img[2]: 兩圖 Matching結果圖
+            keypoint_data[0]: 左圖檢測到的特徵點數量
+            keypoint_data[1]: 右圖檢測到的特徵點數量
+            keypoint_data[2]: Matching檢測到的相同特徵點數量
+            used_time[0]: 左圖 detect時間
+            used_time[1]: 右圖 detect時間
+            used_time[2]: 兩圖 Matching時間
+            Matching accuracy: round(keypoint_data[2]/keypoint_data[0]*100, 2) + "%"
+            """
+            # 不取matching結果
+            _, buffer_left = cv2.imencode('.jpg', result_img[0])
+            _, buffer_right = cv2.imencode('.jpg', result_img[1])
+            image_str_left = base64.b64encode(buffer_left).decode('utf-8')
+            image_str_right = base64.b64encode(buffer_right).decode('utf-8')
+            content = {"processedImageLeft": image_str_left,
+                    "processedImageLeft": image_str_right,
+                    "keypointNumLeft":keypoint_data[0],
+                    "keypointNumRight":keypoint_data[1],
+                    "detectTimeLeft":used_time[0],
+                    "detectTimeRight":used_time[1],
+                    }
+            
+            time.sleep(response_wait_time)
+            
+            return JSONResponse(content)
+            ...
+        # 論文實驗2：調整輸入圖像的亮度，並查看特徵點比對數據
+        elif (experiment == "exp2"):
+
+            orbProcessor = orb_module.OrbProcessor()
+            orbProcessor.setORBProcessor(config_data)
+            result_img, keypoint_data, used_time = orbProcessor.compareToDiffBrightness(brightness_adj = bright_adj)
+            """
+            result_img[0]: 左圖 detect結果圖
+            result_img[1]: 右圖 detect結果圖
+            result_img[2]: 兩圖 Matching結果圖
+            keypoint_data[0]: 左圖檢測到的特徵點數量
+            keypoint_data[1]: 右圖檢測到的特徵點數量
+            keypoint_data[2]: Matching檢測到的相同特徵點數量
+            used_time[0]: 左圖 detect時間
+            used_time[1]: 右圖 detect時間
+            used_time[2]: 兩圖 Matching時間
+            Matching accuracy: round(keypoint_data[2]/keypoint_data[0]*100, 2) + "%"
+            """            
+            # 取matching結果
+            _, buffer_left = cv2.imencode('.jpg', result_img[0])
+            _, buffer_right = cv2.imencode('.jpg', result_img[1])
+            _, buffer_match = cv2.imencode('.jpg', result_img[2])
+            image_str_left = base64.b64encode(buffer_left).decode('utf-8')
+            image_str_right = base64.b64encode(buffer_right).decode('utf-8')
+            image_str_match = base64.b64encode(buffer_match).decode('utf-8')
+            content = {"processedImageLeft": image_str_left,
+                    "processedImageLeft": image_str_right,
+                    "processedImageMatch": image_str_match,
+                    "keypointNumLeft":keypoint_data[0],
+                    "keypointNumRight":keypoint_data[1],
+                    "keypointNumMatch":keypoint_data[2],
+                    "detectTimeLeft":used_time[0],
+                    "detectTimeRight":used_time[1],
+                    "detectTimeRight":used_time[2],
+                    }
+            
+            time.sleep(response_wait_time)
+            
+            return JSONResponse(content)
+            ...
+        # 查看單張圖的效果
+        elif (experiment == "exp3"):
+            orbProcessor = orb_module.OrbProcessor()
+            orbProcessor.setORBProcessor(config_data)
+            result_img, keypoint_num, detect_time = orbProcessor.detectOrbFeature()
+            print("Features : " + str(keypoint_num))
+            print("Time : " + str(detect_time) + "s")
+
+            _, buffer = cv2.imencode('.jpg', result_img)
+            image_str = base64.b64encode(buffer).decode('utf-8')
+            content = {"processedImage": image_str,
+                    "keypointNum":keypoint_num,
+                    "detectTime":detect_time
+                    }
+            
+            time.sleep(response_wait_time)
+            
+            return JSONResponse(content)
     
     except Exception as e:
         return {'error': str(e)}
